c2200d25356a376a6270191e2149889732e21a9c
kirkasmusta <viskrebets@gmail.com>
2021-10-18 21:25:21 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.slider').slick({
         dots:true,
     }
     );
-});
+}); */
+
+
+function triangleIso (stars) {
+
+        for (var i = 0; i < stars; i++) {
+        var res = '';
+        for (var j = 0; j < (stars - (i * 2) - 1 ) / 2; j++) {
+            res += '+';
+        }
+        for (var j = 0; j < (i + 1) * 2 - 1 < stars; j++) {
+
+            if (j < stars) {
+            res += '*';
+        } }
+
+        console.log(res)
+    }
+}
+
+triangleIso(5);
