e848704ddb3f19dcf002b9a9c99bd8632482bfb8
kirkasmusta <viskrebets@gmail.com>
2021-10-20 18:05:07 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,30 @@
-/* $(document).ready(function(){
-    $('.slider').slick({
-        dots:true,
-    }
-    );
-}); */
-
-
 function triangleIso (stars) {
 
-        for (var i = 0; i < stars; i++) {
+    for (var i = 0; i < (stars + 1) / 2; i++) {
         var res = '';
-        for (var j = 0; j < (stars - (i * 2) - 1 ) / 2; j++) {
-            res += '+';
-        }
-        for (var j = 0; j < (i + 1) * 2 - 1 < stars; j++) {
 
-            if (j < stars) {
-            res += '*';
-        } }
+        for (var j = 0; j < (stars - (i * 2) - 1 ) / 2; j++) {
+            res += ' ';
+        }
+        for (var j = 0; j < (i + 1) * 2 - 1 && j < stars; j++) {
+            res += "*";
+        }
+
+        console.log(res)
+    }
+
+    for (var i = 0; i < (stars - 1) / 2; i++) {
+        var res = '';
+
+        for (var j = 0; j < i + 1; j++) {
+            res += ' ';
+        }
+        for (var j = 0; j < stars - (i * 2) - 2; j++) {
+            res += "*";
+        }
 
         console.log(res)
     }
 }
 
-triangleIso(5);
+triangleIso(7);
